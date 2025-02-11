a4efbf220dc1276b2e4c4d4bf89ba3c0bd2a1765
Kiran <mkkiran835@gmail.com>
2025-02-11 12:19:53 +0530

    Port updated

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 dotenv.config();
 
